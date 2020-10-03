9d3234c53aebee9be6eda856b5151d411ee8121d
cmaghas <coachofanalytics@gmail.com>
2020-10-03 12:38:20 -0500

    deleted @login required for training template

--- data/views.py
+++ data/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 def course(request):
     return render(request, 'data/course.html', {'title': 'course'})
 
-@login_required  
+ 
 def training(request):
     return render(request, 'data/training.html', {'title': 'training'})
 
