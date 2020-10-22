fbdd6325f28c637b116dc7d789622f8f05561eff
cmaghas <coachofanalytics@gmail.com>
2020-10-22 07:51:36 -0500

    added django-heroku

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,8 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codatrainingapp.herokuapp.com']
-#ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['codatrainingapp.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -169,3 +170,4 @@ AWS_S3_FILE_OVERWRITE=False
 AWS_DEFAULT_ACL=None
 DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 #STATICFILES_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
+django_heroku.settings(locals())
