c1649b5dc1732e3816bded07c8f3e3738b7f39ed
cmaghas <coachofanalytics@gmail.com>
2020-10-30 09:56:43 -0500

    remove company documentations

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 #from .models import Codadoc
-from .forms import CodadocumentsForm
+#from .forms import CodadocumentsForm
 # Create your views here.
 def test(request):
     return render(request, 'main/test.html', {'title': 'test'})
