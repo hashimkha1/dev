a710edd06f6b7b452b7dcced215033199edaddf2
cmaghas <coachofanalytics@gmail.com>
2021-02-24 19:22:26 -0500

    deleted copies

--- application - Copy/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Applicant_Profile,Application,Rated,Employee,FirstUpload
-# Register your models here.
-admin.site.register(Applicant_Profile)
-admin.site.register(Application)
-admin.site.register(Rated)
-admin.site.register(Employee)
-admin.site.register(FirstUpload)
