e13f35d0876a59e6d41b1e43ce404e8cf0bf8349
cmaghas <coachofanalytics@gmail.com>
2021-02-28 03:35:57 -0500

    settings to False

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG =(os.environ.get('DEBUG_VALUE')=='True')
+#DEBUG =(os.environ.get('DEBUG_VALUE')=='True')
+DEBUG =(os.environ.get('DEBUG_VALUE')=='False')
 
 
 #ALLOWED_HOSTS = ['codaappdev.herokuapp.com']
