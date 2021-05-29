a22791a87ed28a54c73b49ae4d72beee4af07c49
cmaghas <coachofanalytics@gmail.com>
2021-05-28 21:34:19 -0400

    added connection

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'coda_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -112,7 +112,7 @@ DATABASES = {
         
     }
 }
-'''
+
 import dj_database_url
 db_from_env=dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
