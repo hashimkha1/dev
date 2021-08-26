43b305c92bfc91e730934915797eeb9347f039f0
CHRISTOPHER MAGHAS <coachofanalytics@gmail.com>
2021-08-26 03:38:13 -0500

    added manual secrete key

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -20,14 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY ='!cxl7yhjsl00964n=#e-=xblp4u!hbajo2k8u#$v9&s6__5=xf'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY ='!cxl7yhjsl00964n=#e-=xblp4u!hbajo2k8u#$v9&s6__5=xf'
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'False'
 #DEBUG =(os.environ.get('DEBUG_VALUE')=='True')
-#ALLOWED_HOSTS = ['codaappdev.herokuapp.com']
-ALLOWED_HOSTS = ['127.0.0.1','localhost','codatrainingapp.herokuapp.com','www.codanalytics.net','codanalytics.net']
+ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['127.0.0.1','localhost','codatrainingapp.herokuapp.com','www.codanalytics.net','codanalytics.net']
 #ALLOWED_HOSTS = []
 AUTH_USER_MODEL='accounts.CustomerUser'
 AUTHENTICATION_BACKENDS = (
