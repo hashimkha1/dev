83bd935c015f72bdc675ca46e4f04f6fe425624f
CHRISTOPHER MAGHAS <coachofanalytics@gmail.com>
2021-09-18 22:15:01 -0500

    Expense modules and store app

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -108,15 +108,16 @@ DATABASES = {
     }
 }
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CODA_UAT',# Name of Database
+        'USER':'postgres',
+        'PASSWORD': 'MANAGER#2030', #os.environ.get('POSTGRESSPASS'),
+        'HOST': 'localhost',
     }
 }
-
-
 
 
 DATABASES = {
@@ -131,16 +132,14 @@ DATABASES = {
 
 '''
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CODA_UAT',# Name of Database
-        'USER':'postgres',
-        'PASSWORD': 'MANAGER#2030', #os.environ.get('POSTGRESSPASS'),
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        
     }
 }
+
 
 import dj_database_url
 db_from_env=dj_database_url.config(conn_max_age=600)
