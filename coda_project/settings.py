aea47defa72992c2540b90ac819294588cbc911a
CHRISTOPHER MAGHAS <coachofanalytics@gmail.com>
2022-01-22 22:10:35 -0600

    changed db

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -154,8 +154,6 @@ DATABASES = {
     }
 }
 
-'''
-import dj_database_url
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -163,6 +161,19 @@ DATABASES = {
         
     }
 }
+'''
+import dj_database_url
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CODA_DEV',# Name of Database
+        'USER':'CODA_DEV',
+        'PASSWORD': os.environ.get('POSTGRESSPASS'),
+        'HOST': 'database-1.ckq8mwyj2m9n.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
+    }
+}
+
 db_from_env=dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
