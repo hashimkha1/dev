07a351e4803ddeeef3d8cfa5a831d2e25147b264
CHRISTOPHER MAGHAS <coachofanalytics@gmail.com>
2022-01-23 02:30:44 -0600

    Changed db to sqlite

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -154,23 +154,13 @@ DATABASES = {
     }
 }
 
+'''
+import dj_database_url
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         
-    }
-}
-'''
-import dj_database_url
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CODA_DEV',# Name of Database
-        'USER':'CODA_DEV',
-        'PASSWORD': os.environ.get('POSTGRESSPASS'),
-        'HOST': 'database-1.ckq8mwyj2m9n.us-east-2.rds.amazonaws.com',
-        'PORT': '5432'
     }
 }
 
