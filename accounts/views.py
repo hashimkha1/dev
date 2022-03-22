ce47cb20cd3940cca74e17af657780aa0479cd89
Chris Maghas <coachofanalytics@gmail.com>
2022-03-22 17:30:28 -0500

    made changes to accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 import datetime
 from datetime import date ,timedelta
-from types import NoneType
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
