267ec4be19774a8fd536abc383fa4c2a8431c09a
Chris Maghas <coachofanalytics@gmail.com>
2022-05-10 13:14:09 -0500

    change password

--- coda_project/urls.py
+++ coda_project/urls.py
@@ -23,7 +23,7 @@ from django.views.static import serve
 from django.contrib.auth import views as auth_views
 
 from accounts import views as account_views
-from accounts.views import PasswordsChangeView ,PasswordsSetView #,account_views
+#from accounts.views import account_views PasswordsChangeView ,PasswordsSetView,
 from coda_project import settings
 from testing import AdminViews, views
 
@@ -48,8 +48,8 @@ urlpatterns = [
     path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='accounts/registration/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/registration/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/registration/password_reset_complete.html'), name='password_reset_complete'),
-    path('password-change/', PasswordsChangeView.as_view(),{'post_change_redirect': 'accounts:password_change_done'}, name='password_change'),
-    path('password-set/', PasswordsSetView.as_view(template_name='accounts/registration/password_set_form.html'), name='password_set'),
+    #path('password-change/', PasswordsChangeView.as_view(),{'post_change_redirect': 'accounts:password_change_done'}, name='password_change'),
+    #path('password-set/', PasswordsSetView.as_view(template_name='accounts/registration/password_set_form.html'), name='password_set'),
     #path('reset-password/', account_views.reset_password, name='reset-password'),
     
     #path(r'^password_change/$',auth_views.password_change, {'post_change_redirect': 'league:password_change_done'},
