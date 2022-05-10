1aeaf63a2813bebaf4c794a5149d4e50498739cc
Chris Maghas <coachofanalytics@gmail.com>
2022-05-10 14:02:17 -0500

    reset complete view

--- coda_project/urls.py
+++ coda_project/urls.py
@@ -47,7 +47,9 @@ urlpatterns = [
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='accounts/registration/password_reset.html'), name='password_reset'),
     path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='accounts/registration/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/registration/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/registration/password_reset_complete.html'), name='password_reset_complete'),
+    #path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/registration/password_reset_complete.html'), name='password_reset_complete'),
+    path('password-reset-complete/', account_views.PasswordResetCompleteView, name='password_reset_complete'),
+   
     #path('password-change/', PasswordsChangeView.as_view(),{'post_change_redirect': 'accounts:password_change_done'}, name='password_change'),
     #path('password-set/', PasswordsSetView.as_view(template_name='accounts/registration/password_set_form.html'), name='password_set'),
     #path('reset-password/', account_views.reset_password, name='reset-password'),
