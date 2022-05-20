e1a16f3f4ff9e08dd0f376c0d3709abfdd895df8
Chris Maghas <coachofanalytics@gmail.com>
2022-05-20 11:46:49 -0500

    made changes to seetings file

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -250,12 +250,11 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
 
 # Other Email Backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail.privateemail.com'
+EMAIL_HOST = "mail.privateemail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'info@codanalytics.net'
-EMAIL_HOST_PASSWORD ='@ZK123sebe'
-
+EMAIL_HOST_USER = "Info@codanalytics.net"
+EMAIL_HOST_PASSWORD = "@ZK123sebe"
 
 
 AWS_S3_REGION_NAME = 'us-east-2' #change to your region
