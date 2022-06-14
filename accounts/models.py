5c62f60a59f089c46f293b31cba10f8ddc919fbb
narevit <yalamanchinarendra999@gmail.com>
2022-06-14 12:39:42 -0400

    removing plan in models

--- accounts/models.py
+++ accounts/models.py
@@ -146,12 +146,12 @@ class Tracker(models.Model):
         max_length=25,
         choices=TASK_CHOICES,
     )
-    plan = models.CharField(
-        verbose_name=_('group'),
-        help_text=_('Required'),
-        max_length=255,
-        default="B"
-        )
+    # plan = models.CharField(
+    #     verbose_name=_('group'),
+    #     help_text=_('Required'),
+    #     max_length=255,
+    #     default="B"
+    #     )
     author = models.ForeignKey('accounts.CustomerUser', on_delete=models.CASCADE, related_name="author")
     clientname = models.ForeignKey('accounts.CustomerUser', on_delete=models.CASCADE, related_name="clientname",limit_choices_to={'is_client': True})
     author = models.ForeignKey("accounts.CustomerUser", on_delete=models.CASCADE)
