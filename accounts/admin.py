40e776c44721f51ab365fc31a552035a5b1eebf4
kneeraazon01 <kneeraazon@gmail.com>
2022-06-20 00:39:22 +0545

    resume field issue fixed

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,33 +1,66 @@
-
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
-from .models import CustomerUser, Tracker #, Profile
+from .models import CustomerUser, Tracker  # , Profile
 
 
-#admin.site.register(CustomerUser)
+# admin.site.register(CustomerUser)
 class CustomerAdmin(UserAdmin):
-    add_form=UserCreationForm
-    form=UserChangeForm
-    list_display=('email','first_name','last_name')
+    add_form = UserCreationForm
+    form = UserChangeForm
+    list_display = ("email", "first_name", "last_name")
 
-
-    fieldsets=UserAdmin.fieldsets +(
-        (None, {'fields': ('gender','phone','address','city','state','country','is_admin','is_employee','is_client','is_applicant')}),
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            None,
+            {
+                "fields": (
+                    "gender",
+                    "phone",
+                    "address",
+                    "city",
+                    "state",
+                    "country",
+                    "is_admin",
+                    "is_employee",
+                    "is_client",
+                    "is_applicant",
+                    "resume_file",
+                )
+            },
+        ),
     )
 
-    add_fieldsets=UserAdmin.add_fieldsets +(
-        (None, {'fields': ('email', 'first_name','last_name','password1','password2','gender','category','phone','address','city','state','country','is_admin')}),
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "gender",
+                    "category",
+                    "phone",
+                    "address",
+                    "city",
+                    "state",
+                    "country",
+                    "is_admin",
+                    "resume_file",
+                )
+            },
+        ),
     )
-    search_fields = ('email',)
+    search_fields = ("email",)
     filter_horizontal = ()
+
 
 # Now register the new UserAdmin...
 admin.site.register(CustomerUser, CustomerAdmin)
 
 # Register your models here.
 admin.site.register(Tracker)
-
-
