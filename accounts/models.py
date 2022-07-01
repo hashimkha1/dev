eb4e4f71d76e6122c649a7018b7fba4bff14b7a2
CHRISTOPHER MAGHAS <coachofanalytics@gmail.com>
2022-07-01 08:04:05 -0500

    imported sum method from models in account app

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from decimal import *
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import Sum
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
