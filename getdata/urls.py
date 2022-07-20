27de5f95dcc77470930f23345e90260d33725220
Chris Maghas <coachofanalytics@gmail.com>
2022-07-20 01:45:34 -0500

    commenting gotomeeting

--- getdata/urls.py
+++ getdata/urls.py
@@ -7,12 +7,12 @@ app_name = 'getdata'
 urlpatterns = [
     path('getrating/', views.getrating, name='data-getrating'),
     path('index/', views.index, name='data-index'),
-    path('gotomeeting/',views.meetingFormView,name='meetingform1'),
-    # trying a url pattern for dates
-    # gotomeetingresult
-    path('gotomeetingresult/',views.gotomeetingresult,name='gotomeetingresult'),
-    #meeting detail
-    re_path(r'^gotomeeting/(?P<meeting_id>[0-9]+)$', views.meetingView6, name='gotomeetingmeeting')
+#     path('gotomeeting/',views.meetingFormView,name='meetingform1'),
+#     # trying a url pattern for dates
+#     # gotomeetingresult
+#     path('gotomeetingresult/',views.gotomeetingresult,name='gotomeetingresult'),
+#     #meeting detail
+#     re_path(r'^gotomeeting/(?P<meeting_id>[0-9]+)$', views.meetingView6, name='gotomeetingmeeting')
 
 
 ]
