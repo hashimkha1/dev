5816722b7ddc91af680856939872e7c3c338ff16
Chris Maghas <coachofanalytics@gmail.com>
2022-08-29 17:41:03 -0500

    testing cheanges

--- testing/views.py
+++ testing/views.py
@@ -31,6 +31,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from finance.models import Transaction,Inflow,TrainingLoan
 from accounts.models import Tracker,Department, TaskGroups
+from management.models import Task
 from coda_project import settings
 from datetime import date, timedelta
 from django.db.models import Q
@@ -113,7 +114,7 @@ def task_url():
         #     print("year")
         # else:
         #     print("no")
-task_url()
+# task_url()
 @register.filter(name='activitieslist')
 def activitieslist(value, myactivities):
     return True if value in myactivities else False
