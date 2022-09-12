9b94d33786fd53ef58deaed57fc5bf898fe8d169
Chris Maghas <coachofanalytics@gmail.com>
2022-09-11 21:55:15 -0500

    email functionality

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -134,25 +134,25 @@ import dj_database_url
 #     }
 # }
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",  # Name of Database
-#         "USER": "postgres",
-#         "PASSWORD": "MANAGER2030",  # os.environ.get('POSTGRESSPASS'),
-#         "HOST": "localhost",
-#     }
-# }
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "das6459qgcjbmv",  # Name of Database
-        "USER": "xgsxxbmwywwmoj",
-        "PASSWORD": "91b741ac954fc9e9e10ad57c0916d2b57a3964ede1c97d58c83dfa7f966a82f1",  # os.environ.get('POSTGRESSPASS'),
-        "HOST": "ec2-3-223-169-166.compute-1.amazonaws.com",
+        "NAME": "postgres",  # Name of Database
+        "USER": "postgres",
+        "PASSWORD": "MANAGER2030",  # os.environ.get('POSTGRESSPASS'),
+        "HOST": "localhost",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "das6459qgcjbmv",  # Name of Database
+#         "USER": "xgsxxbmwywwmoj",
+#         "PASSWORD": "91b741ac954fc9e9e10ad57c0916d2b57a3964ede1c97d58c83dfa7f966a82f1",  # os.environ.get('POSTGRESSPASS'),
+#         "HOST": "ec2-3-223-169-166.compute-1.amazonaws.com",
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
