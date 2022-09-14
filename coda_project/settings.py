7d88949a058ef50150f5aed8fd0a9e38f22b05eb
Pall Pandiyan <pallpandiyan.s@egrovesys.com>
2022-09-14 14:41:57 +0530

    made the STATIC_ROOT to point to '/staticfiles' instead of 'coda_project/staticfiles', and runned the 'python manage.py collectstatic' to generate the static files.

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -210,7 +210,7 @@ USE_TZ = True
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, '..', "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIR = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
