446c240d3aafada18d161227350f67e3bdd44efe
Pall Pandiyan <pallpandiyan.s@egrovesys.com>
2022-09-19 15:28:58 +0530

    temporarily change the loan cron to run every 1 min..

--- coda_project/celery.py
+++ coda_project/celery.py
@@ -23,6 +23,7 @@ app.conf.beat_schedule = {
     },
     'loan_amount_detection': {
         'task': 'TrainingLoanDetectionHistory',
-        'schedule': crontab(day_of_month=28),
-    }, 
+        'schedule': crontab(minute='*/1'),
+        # 'schedule': crontab(day_of_month=28),
+    },
 }
