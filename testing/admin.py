6f72675d4d529ff1f8353af9731a9864601a380e
Chris Maghas <coachofanalytics@gmail.com>
2022-10-26 03:53:32 -0500

    adding food functionalities

--- testing/admin.py
+++ testing/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import (
     Services,
+    Supplier,
+    Food,
 )
 # Register your models here.
 admin.site.register(Services)
+admin.site.register(Supplier)
+admin.site.register(Food)
