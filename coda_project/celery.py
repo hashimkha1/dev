56bbda55ce514d206879e9f3bdbf0b42568ffb1b
pythondev1 <pythondev@ongraph.com>
2022-11-10 17:19:00 +0530

    update

--- coda_project/celery.py
+++ coda_project/celery.py
@@ -26,7 +26,7 @@ app.conf.beat_schedule = {
     'replies_mails': {
         'task': 'replies_job_mail',
         # 'schedule': crontab(minute='*/1'),
-        'schedule': crontab(minute=3),
+        'schedule': crontab(hour=23),
     },
 
 }
