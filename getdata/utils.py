9cb04dafd0cc14b604bbec75a72367093c644256
Mahtab Dogar <mahtabdogar03@gmail.com>
2023-02-23 03:10:13 +0500

    change for chrome driver

--- getdata/utils.py
+++ getdata/utils.py
@@ -4,8 +4,6 @@ import re
 from dateutil import parser
 from bs4 import BeautifulSoup
 import psycopg2
-import chromedriver_autoinstaller as chromedriver
-
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
