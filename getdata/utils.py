35c4670563cb06ca04f314416423c3dc5ee35970
Mahtab Dogar <mahtabdogar03@gmail.com>
2023-02-26 03:21:49 +0500

    requirements.txt

--- getdata/utils.py
+++ getdata/utils.py
@@ -558,17 +558,17 @@ def main_shortput():
     # # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
     # driver = webdriver.Chrome(options=options)
 
-    # service_object = Service(binary_path)
-    # driver = webdriver.Chrome(service=service_object)
-    import os
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver.get("https://medium.com")
-    print(driver.page_source)
+    service_object = Service(binary_path)
+    driver = webdriver.Chrome(service=service_object)
+    # import os
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    # driver.get("https://medium.com")
+    # print(driver.page_source)
     return driver.page_source
     # print("Finished!")
 
