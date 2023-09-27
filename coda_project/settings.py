45fca66c0e7fb2263f95eaa8ac53bffe84365a6b
Developer Use <amit_kumar@esferasoft.com>
2023-09-27 14:39:49 +0530

    debuge off case

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -237,7 +237,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_ROOT = os.path.join(BASE_DIR, '..', "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIR = os.path.join(BASE_DIR, "static")
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
