4d1006583e5201256ab4f9295cbafdd421614989
CHRISTOPHER MAGHAS <coachofanalytics@gmail.com>
2023-10-02 00:59:42 -0500

    coda_project/celery.py

--- coda_project/celery.py
+++ coda_project/celery.py
@@ -11,7 +11,7 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'run_on_every_1st': {
         'task': 'task_history',
-        'schedule': crontab(0, 0, day_of_month='1'),
+        'schedule': crontab(0, 0, day_of_month='2'),
     },
 
     'login_no_activity_send_sms': {
