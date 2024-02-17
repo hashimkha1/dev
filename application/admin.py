2f60dd154868c45375f1f9d7041e4a6790b764f9
unknown <coachofanalytics@gmail.com>
2024-02-17 13:37:01 +0300

    created wcag model on the application app

--- application/admin.py
+++ application/admin.py
@@ -1,10 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# from .models import UserProfile, Application, Policy, Rated, Reporting
+from .models import WCAG_STANDARD_WEBSITE
 
-# # Register your models here.
+#----------- Register your models here.
 # admin.site.register(UserProfile)
 # admin.site.register(Application)
 # admin.site.register(Rated)
 # admin.site.register(Policy)
 # admin.site.register(Reporting)
+admin.site.register(WCAG_STANDARD_WEBSITE)
--- application/models.py
+++ application/models.py
@@ -1,12 +1,19 @@
 # # from coda_project.application.views import first_interview
-# from datetime import datetime
+from datetime import datetime
+from django.utils import timezone
 # from distutils.command.upload import upload
 
-# from django.db import models
+from django.db import models
 # from django.utils import timezone
 # from accounts.models import CustomerUser
 # from main.models import Assets
 # from django.db.models import Q
 # from coda_project.storage import GoogleDriveStorage
 
-#
+class WCAG_STANDARD_WEBSITE(models.Model):
+    website_url = models.CharField(max_length=500)
+    page_name = models.CharField(max_length=500)
+    improvements = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
