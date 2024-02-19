c5525c1141470bb50ec99a2195c34c89f37e8ddd
coachofanalytics <coachofanalytics@gmail.com>
2024-02-19 18:02:39 +0300

    created company_propertyupdate view on application

--- application/urls.py
+++ application/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-from .views import company_propertiesList,company_propertiesCreate
+from .views import company_propertiesList,company_propertiesCreate,company_properties_update
 app_name = 'application'
 urlpatterns = [
     # path('application/', include('application.urls')),
@@ -8,7 +8,9 @@ urlpatterns = [
     #path('', views.home, name='home'),
 
     path('propertylist/', views.company_propertiesList, name='propertylist'),
-    path('propertycreate/', views.company_propertiesCreate, name='propertycreate')
+    path('propertycreate/', views.company_propertiesCreate, name='propertycreate'),
+    path('propertyupdate/<int:pk>/',views.company_properties_update,name='propertyupdate'),
+
 
 
 ]
--- application/views.py
+++ application/views.py
@@ -38,3 +38,14 @@ def company_propertiesCreate(request):
         form = propertiesForm()
         print("form ====>",form) 
     return render(request,"application/propertycreate.html", {'form': form})
+
+def company_properties_update(request,pk):
+    properties = get_object_or_404(company_properties,pk=pk)
+    if request.method =='POST':
+        form =propertiesForm(request.POST,instance = properties)
+        if form.is_valid():
+            form.save()
+            return redirect("application:propertylist")
+    else:
+        form = propertiesForm(instance= properties)
+        return render(request,"application/propertyupdate.html",{'form':form,'properties':properties})
