8f4c1f0e4465b2deee274aed87d48d3ccb889b9e
coachofanalytics <coachofanalytics@gmail.com>
2024-02-21 12:01:36 +0300

    created wcagWebsite model on application app

--- application/models.py
+++ application/models.py
@@ -1,12 +1,40 @@
-# # from coda_project.application.views import first_interview
-# from datetime import datetime
-# from distutils.command.upload import upload
+from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.text import slugify
+from random import randint
+# # Create your models here.
+from django.contrib.auth import get_user_model
+# from finance.utils import get_exchange_rate
+User = get_user_model() 
 
-# from django.db import models
-# from django.utils import timezone
-# from accounts.models import CustomerUser
-# from main.models import Assets
-# from django.db.models import Q
-# from coda_project.storage import GoogleDriveStorage
 
-#
+
+
+
+
+class wcagsWebsite(models.Model):
+    website_url  = models.CharField(max_length=500)
+    page_name    = models.CharField(max_length=500)
+    improvements = models.TextField(null=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.website_url
+
+
+
+
+#class group_task(models.Model):
+     #title = models.CharField(max_length=55)
+     #description = models.TextField()
+     #created_at = models.DateTimeField(auto_now_add=True)
+   
+ 
+
+    # def __str__(self):
+    #     return self.name
+
+
+ 
