fcd95e9642ce9a1fe55abcb57c5e73ca7b2b6604
coachofanalytics <coachofanalytics@gmail.com>
2024-02-22 02:40:31 +0300

    created a codawcags model on the application app

--- application/admin.py
+++ application/admin.py
@@ -1,10 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# from .models import UserProfile, Application, Policy, Rated, Reporting
+from .models import work_department,codawcags
 
 # # Register your models here.
 # admin.site.register(UserProfile)
 # admin.site.register(Application)
 # admin.site.register(Rated)
-# admin.site.register(Policy)
-# admin.site.register(Reporting)
+admin.site.register(codawcags)
+admin.site.register(work_department)
