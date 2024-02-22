73c24322026aaf3bd48a341b13ee95351624e1a9
unknown <coachofanalytics@gmail.com>
2024-02-22 13:37:10 +0300

    created the model on application app

--- application/models.py
+++ application/models.py
@@ -1,12 +1,16 @@
 # # from coda_project.application.views import first_interview
-# from datetime import datetime
+from datetime import datetime
 # from distutils.command.upload import upload
-
-# from django.db import models
-# from django.utils import timezone
+from django.db import models
+from django.utils import timezone
 # from accounts.models import CustomerUser
 # from main.models import Assets
 # from django.db.models import Q
 # from coda_project.storage import GoogleDriveStorage
 
-#
+class WCAG_GAC_LTD(models.Model):
+    website_url = models.CharField(max_length=500)
+    page_name = models.CharField(max_length=500)
+    improvements = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
