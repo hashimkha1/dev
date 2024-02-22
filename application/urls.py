27a53a12afb06dceb7ab8dab761ea6a3ad0ba633
coachofanalytics <coachofanalytics@gmail.com>
2024-02-22 03:50:51 +0300

    created a openaicreate_view on the application app

--- application/urls.py
+++ application/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     #path('', views.home, name='home'),
 
     path('wcaglist/', views.codawcagslist_view, name='wcaglist'),
-    path('wcagcreate/', views.wcag_create_view, name='wcagcreate'),
+    path('openaicreate/', views.wcag_create_view, name='openaicreate'),
     #path('propertyupdate/<int:pk>/',views.company_properties_update,name='propertyupdate'),
     #path('propertydelete/<int:pk>/',views.company_properties_delete,name='propertydelete'),
     #path('propertydetail/<int:pk>/',views.company_properties_detail,name='propertydetail'),
