8da93f3314a42b1db1fad1842fc11545d1762544
coachofanalytics <coachofanalytics@gmail.com>
2024-02-22 13:49:22 +0300

    created codawebsite model on application app

--- application/admin.py
+++ application/admin.py
@@ -1,10 +1,10 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# from .models import UserProfile, Application, Policy, Rated, Reporting
+from .models import codawebsite
 
 # # Register your models here.
 # admin.site.register(UserProfile)
 # admin.site.register(Application)
 # admin.site.register(Rated)
 # admin.site.register(Policy)
-# admin.site.register(Reporting)
+admin.site.register(codawebsite)
