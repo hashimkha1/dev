4629a16eb49aa5315d682737227b2d9a27cb8e76
coachofanalytics <coachofanalytics@gmail.com>
2024-02-24 03:39:23 +0300

    created websitewcag model on the application app

--- application/admin.py
+++ application/admin.py
@@ -1,10 +1,8 @@
-# from django.contrib import admin
-
-# from .models import UserProfile, Application, Policy, Rated, Reporting
-
+from django.contrib import admin
+from .models import websitewcag
 # # Register your models here.
 # admin.site.register(UserProfile)
 # admin.site.register(Application)
 # admin.site.register(Rated)
 # admin.site.register(Policy)
-# admin.site.register(Reporting)
+admin.site.register(websitewcag)
--- application/models.py
+++ application/models.py
@@ -1,12 +1,29 @@
-# # from coda_project.application.views import first_interview
-# from datetime import datetime
-# from distutils.command.upload import upload
+from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.text import slugify
+from random import randint
+# # Create your models here.
+from django.contrib.auth import get_user_model
+# from finance.utils import get_exchange_rate
+User = get_user_model() 
 
-# from django.db import models
-# from django.utils import timezone
-# from accounts.models import CustomerUser
-# from main.models import Assets
-# from django.db.models import Q
-# from coda_project.storage import GoogleDriveStorage
 
-#
+
+# class work_department(models.Model):
+#     name = models.CharField(max_length=100, null=False)
+#     slug = models.SlugField(max_length=255, null=False)
+#     is_featured = models.BooleanField(default=False, null=False)
+#     is_active = models.BooleanField(default=False, null=False)
+
+
+
+class websitewcag(models.Model):
+    website_url  = models.CharField(max_length=500)
+    page_name    = models.CharField(max_length=500)
+    improvements = models.TextField(null=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.website_url
