9a00714b43b8db6cf92dfb29608db322a0cf1b4d
coachofanalytics <coachofanalytics@gmail.com>
2024-02-27 09:25:59 +0300

    created Balance_sheetcategory model on the application app

--- application/admin.py
+++ application/admin.py
@@ -1,10 +1,9 @@
-# from django.contrib import admin
-
-# from .models import UserProfile, Application, Policy, Rated, Reporting
+from django.contrib import admin
+from .models import Balance_sheetCategory
 
 # # Register your models here.
 # admin.site.register(UserProfile)
 # admin.site.register(Application)
 # admin.site.register(Rated)
 # admin.site.register(Policy)
-# admin.site.register(Reporting)
+admin.site.register(Balance_sheetCategory)
--- application/models.py
+++ application/models.py
@@ -1,12 +1,29 @@
 # # from coda_project.application.views import first_interview
-# from datetime import datetime
+from datetime import datetime
 # from distutils.command.upload import upload
 
-# from django.db import models
+from django.db import models
 # from django.utils import timezone
 # from accounts.models import CustomerUser
 # from main.models import Assets
-# from django.db.models import Q
+from django.db.models import Q,F
 # from coda_project.storage import GoogleDriveStorage
 
-#
+#create models here
+
+class Balance_sheetCategory(models.Model):
+    CATEGORY_TYPES = [
+        ("assets", "Asset"),
+        ("liability", "Liability"),
+        ("equity", "Equity"),
+    ]
+    name = models.CharField(max_length=255)
+    category_type = models.CharField(max_length=255, choices=CATEGORY_TYPES, default='assets')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        unique_together = (('name', 'category_type'),)
+        verbose_name_plural = 'Balancesheet Categories'
+
+    def __str__(self):
+        return f"{self.name} ({self.category_type})"
