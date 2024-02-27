9d894d0411228b255a0579da936dfcab7a8d359e
coachofanalytics <coachofanalytics@gmail.com>
2024-02-27 11:21:15 +0300

    created Balance_sheet_Entry model and Balancesheet summary model  on the application app

--- application/admin.py
+++ application/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Balance_sheetCategory
+from .models import Balance_sheetCategory,BalanceSheetSummary,Balance_Sheet_Entry
 
 # # Register your models here.
 # admin.site.register(UserProfile)
 # admin.site.register(Application)
-# admin.site.register(Rated)
-# admin.site.register(Policy)
+admin.site.register(Balance_Sheet_Entry)
+admin.site.register(BalanceSheetSummary)
 admin.site.register(Balance_sheetCategory)
--- application/models.py
+++ application/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 # from distutils.command.upload import upload
 
 from django.db import models
-# from django.utils import timezone
+from django.utils import timezone
 # from accounts.models import CustomerUser
 # from main.models import Assets
 from django.db.models import Q,F
@@ -27,3 +27,63 @@ class Balance_sheetCategory(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.category_type})"
+
+
+
+
+class BalanceSheetSummary(models.Model):
+    date = models.DateField(default=timezone.now)
+
+    @property
+    def total_assets(self):
+        return self.entries.filter(category__category_type='Asset').aggregate(models.Sum('amount'))['amount__sum'] or 0
+
+    @property
+    def total_liabilities(self):
+        return self.entries.filter(category__category_type='Liability').aggregate(models.Sum('amount'))['amount__sum'] or 0
+
+    @property
+    def total_equity(self):
+        return self.entries.filter(category__category_type='Equity').aggregate(models.Sum('amount'))['amount__sum'] or 0
+
+    class Meta:
+        verbose_name_plural = "Balance Sheets"
+        ordering = ["-date"]
+
+    def __str__(self):
+        return f"Balance Sheet as of {self.date}"
+
+class Balance_Sheet_Entry(models.Model):
+    balance_sheet = models.ForeignKey('BalanceSheetSummary', on_delete=models.CASCADE, related_name='entries')
+    category = models.ForeignKey('Balance_SheetCategory', on_delete=models.CASCADE)
+   
+
+    class Meta:
+        verbose_name_plural = "Balance Sheet Entries"
+
+    def __str__(self):
+        return f"{self.category.name}: {self.amount}"
+    
+
+class Balance_Sheet_Entry(models.Model):
+    CATEGORY_TYPES = [
+        ('Asset', 'Asset'),
+        ('Liability', 'Liability'),
+        ('Equity', 'Equity'),
+    ]
+
+    name = models.CharField(max_length=255)
+    category_type = models.CharField(max_length=50, choices=CATEGORY_TYPES, default='Asset')
+
+    balance_sheet = models.ForeignKey('BalanceSheetSummary', on_delete=models.CASCADE, related_name='entries')
+    category = models.ForeignKey('Balance_SheetCategory', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    date = models.DateField(default=timezone.now)
+
+    class Meta:
+        verbose_name_plural = "Balance Sheet Entries"
+
+    def __str__(self):
+        return f"{self.category.name}: {self.amount}"
+
+
