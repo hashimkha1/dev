e853dc8f9bd9d41e9465b143996517abb67920fc
devGlobal <hsk9988660@gmail.com>
2024-03-05 16:43:22 +0500

    changes

--- finance/views.py
+++ finance/views.py
@@ -1439,45 +1439,4 @@ def openai_balancesheet(request):
         # Handle the response and update the context accordingly
 
     # Render the template with the 'assets' variable
-    return render(request, "finance/reports/openai.html", context)
-
-
-
-
-
-def balancesheet(request):
-    try:
-        balance_sheet = BalanceSheetSummary.objects.last()
-        if balance_sheet:
-            # Fetch entries for assets, liabilities, and equity
-            current_assets = balance_sheet.entries.filter(category__category_type='Asset')
-            current_liabilities = balance_sheet.entries.filter(category__category_type='Liability')
-            equity = balance_sheet.entries.filter(category__category_type='Equity')
-
-            # Calculate totals
-            total_assets = current_assets.aggregate(Sum('amount'))['amount__sum'] or 0
-            total_liabilities = current_liabilities.aggregate(Sum('amount'))['amount__sum'] or 0
-            total_equity = equity.aggregate(Sum('amount'))['amount__sum'] or 0
-            total_liabilities_and_equity=total_liabilities+total_equity
-            context = {
-                'company_name': 'CODA',
-                'balance_sheet': balance_sheet,
-                'current_assets': current_assets,
-                'current_liabilities': current_liabilities,
-                'equity': equity,
-                'total_liabilities_and_equity': total_liabilities_and_equity,
-                'total_assets': total_assets,
-                'total_liabilities': total_liabilities,
-                'total_equity': total_equity
-            }
-        else:
-            context = {'error_message': 'No balance sheet data available.'}
-    except Exception as e:
-        print(f"Error: {e}")
-        context = {'error_message': 'An error occurred while fetching balance sheet data.'}
-
-    return render(request, "finance/reports/balancesheet.html", context)
-
-
-
-
+    return render(request, "finance/reports/openai_statements.html", context)
