7ecab654f1ed86531aec17db6a550767ec154014
Chris Maghas <coachofanalytics@gmail.com>
2024-04-13 17:43:00 -0400

    changes

--- application/models.py
+++ application/models.py
@@ -79,7 +79,20 @@ class Balancesheet_entry(models.Model):
         verbose_name_plural='Balance Sheet Entries'
 
     def __str__(self):
-        return f"{self.category.name}:{self.amount}"   
+        return f"{self.category.name}:{self.amount}" 
+
+
+
+class receiver_asset(models.Model):
+    receiver = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=50, null=True)
+    type = models.CharField(max_length=100, null=True)
+    activity_date = models.DateTimeField()
+    receipt_link = models.CharField(max_length=100, null=True)
+    qty = models.DecimalField(max_digits=10, decimal_places=0, null=True)
+
+    def __str__(self):
+        return f"Transaction - {self.activity_date}"      
   
 
 
