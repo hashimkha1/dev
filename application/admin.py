9410b27453607bc1e0f3f118f953785513ca1b52
Bonn <info@CODA@gmail.com>
2024-04-14 02:34:00 +0300

    changes

--- application/admin.py
+++ application/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
-from .models import Balancesheet_categories,Balancesheet_category,Balancesheet_entry,BalanceSheet_Summary
+from .models import XInvestmentContent,XInvestmentStrategy,Balancesheet_categories,Balancesheet_category,Balancesheet_entry,BalanceSheet_Summary
 
 # # Register your models here.
 admin.site.register(Balancesheet_categories)
 admin.site.register(Balancesheet_category)
 admin.site.register(Balancesheet_entry)
 admin.site.register(BalanceSheet_Summary)
+
+admin.site.register(XInvestmentStrategy)
+admin.site.register(XInvestmentContent)
 # admin.site.register(Reporting)
--- application/models.py
+++ application/models.py
@@ -12,6 +12,10 @@ from django.db.models.signals import pre_save, post_save
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
+
+
+from django.utils.text import slugify
+
 #------------create models here----------------
 class Balancesheet_categories(models.Model):
     CATEGORY_TYPES = [
@@ -79,11 +83,47 @@ class Balancesheet_entry(models.Model):
         verbose_name_plural='Balance Sheet Entries'
 
     def __str__(self):
-        return f"{self.category.name}:{self.amount}"   
-  
+        return f"{self.category.name}:{self.amount}"  
 
 
 
 
+class XInvestmentContent(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True, blank=True)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
+class XInvestmentStrategy(models.Model):
+    symbol = models.CharField(max_length=50)
+    action = models.CharField(max_length=50)
+    expiry = models.DateField()
+    strike_price = models.DecimalField(max_digits=10, decimal_places=2)
+    mid_price = models.DecimalField(max_digits=10, decimal_places=2)
+    ask_price = models.DecimalField(max_digits=10, decimal_places=2)
+    implied_volatility_rank = models.DecimalField(max_digits=5, decimal_places=2)
+    earnings_date = models.DateField(null=True, blank=True)
+    earning_flag = models.BooleanField(default=False)
+    stock_price = models.DecimalField(max_digits=10, decimal_places=2)
+    raw_return = models.DecimalField(max_digits=10, decimal_places=2)
+    annualized_return = models.DecimalField(max_digits=10, decimal_places=2)
+    comment = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.symbol
+
+    @property
+    def days_to_expiry(self):
+        """Calculates the days remaining until expiry."""
+        return (self.expiry - timezone.now().date()).days if self.expiry else None
+
+    class Meta:
+        verbose_name_plural = "XInvestment Strategies"
